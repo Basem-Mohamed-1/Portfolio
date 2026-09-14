72959b5c8b2a967029aca1d9eb612831f67eec06
basem mohamed <basemlola949@gmail.com>
2026-09-14 21:51:52 +0300

    Add pixel tracking code on DOMContentLoaded

--- script.js
+++ script.js
@@ -46,6 +46,13 @@
         });
       });
     }
+    // pixel code
+    document.addEventListener("DOMContentLoaded", function() {
+      if (typeof window.oaiq === "function") {
+        window.oaiq("measure", "page_viewed", { type: "contents" });
+      }
+    });
+  
 
     // ---- language toggle (English / Arabic) ----
     var langToggle = document.getElementById('lang-toggle');
